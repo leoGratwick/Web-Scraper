e34f2a039d442e6d48ac9f115e0fa486c504b580
Leo Gratwick <xm22456@bristol.ac.uk>
2023-11-03 18:26:13 +0000

    scrapes wikipedia for top comanies into csv file

--- main.py
+++ main.py
@@ -1,16 +1,47 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+import responses
 
 
-# Press the green button in the gutter to run the script.
+
+def scrapeCompanies():
+    url = "https://en.wikipedia.org/wiki/List_of_largest_companies_by_revenue"
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, "html.parser")
+    table = soup.table.tbody
+    rows = table.find_all("tr")
+
+    data = []
+    for row in rows:
+
+        #print(row)
+        sections = row.find_all("td")
+        #print(sections)
+        dictionary = {}
+        if len(sections) > 6:
+
+            dictionary["Name"] = sections[0].text.strip()
+            dictionary["Type"] = sections[1].text.strip()
+            dictionary["Revenue"] = sections[2].text.strip()
+            dictionary["Profit"] = sections[3].text.strip()
+            dictionary["Employees"] = sections[4].text.strip()
+            dictionary["Country"] = sections[5].text.strip()
+
+            data.append(dictionary)
+
+
+    #print(data)
+
+    df = pd.DataFrame(data)
+    df.index += 1
+    df.to_csv("topCompanies.csv")
+
+
+
+
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    scrapeCompanies()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
