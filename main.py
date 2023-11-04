0d550e34104ccf295226d6a961580f0ce0e8b185
Leo Gratwick <xm22456@bristol.ac.uk>
2023-11-04 00:11:47 +0000

    added easier way to scrap tables with lxml and installed gui for pandas

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pandas as pd
+from pandasgui import show
 import requests
 from bs4 import BeautifulSoup
-import responses
+
 
 
 
@@ -15,10 +16,9 @@ def scrapeCompanies():
     data = []
     for row in rows:
 
-        #print(row)
         sections = row.find_all("td")
-        #print(sections)
         dictionary = {}
+
         if len(sections) > 6:
 
             dictionary["Name"] = sections[0].text.strip()
@@ -30,18 +30,22 @@ def scrapeCompanies():
 
             data.append(dictionary)
 
-
-    #print(data)
-
     df = pd.DataFrame(data)
     df.index += 1
     df.to_csv("topCompanies.csv")
 
+
+def scrapeCompaniesEasy():
+    url = "https://en.wikipedia.org/wiki/List_of_largest_companies_by_revenue"
+    scraper = pd.read_html(url)
+    table = scraper[0]
+    table.sort_index(axis=1).drop("Ref.", axis=1)
+    table.to_csv("topCompaniesEasy.csv", index=False)
 
 
 
 
 
 if __name__ == '__main__':
-    scrapeCompanies()
+    scrapeCompaniesEasy()
 
